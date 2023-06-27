fa66f17da8612c4ebf06aba7c198a2c2f7fd1fb5
Hemanth <81060697+hemu33662@users.noreply.github.com>
2023-06-28 02:13:17 +0530

    Update validate.js

--- icons/php-email-form/validate.js
+++ icons/php-email-form/validate.js
@@ -1,54 +1,21 @@
-<script>
-  // Function to handle form submission
-  function handleSubmit(event) {
-    event.preventDefault(); // Prevent the form from submitting
+document.getElementById('feedback-form').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-    // Validate the form inputs
-    if (validateForm()) {
-      // If the form is valid, proceed with sending the email
-      sendEmail();
-    } else {
-      // If the form is not valid, display error messages or handle accordingly
-      // You can customize this part based on your specific requirements
-      alert('Please fill in all the required fields.');
-    }
-  }
+  var form = this;
+  var formData = new FormData(form);
 
-  // Function to validate the form inputs
-  function validateForm() {
-    var form = document.getElementById('contact-form');
-    return validate(form);
-  }
-
-  // Function to send the email
-  function sendEmail() {
-    // Get the form data
-    var form = document.getElementById('contact-form');
-    var formData = new FormData(form);
-
-    // Perform AJAX request or submit the form to the server for email processing
-    // You can customize this part based on your specific server-side implementation
-    // Example using fetch API:
-    fetch('forms/contact.php', {
-      method: 'POST',
-      body: formData
-    })
-    .then(function(response) {
-      // Handle the response from the server
-      if (response.ok) {
-        alert('Email sent successfully!');
-        form.reset(); // Reset the form after successful submission
+  var xhr = new XMLHttpRequest();
+  xhr.open(form.method, 'send_feedback.php', true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var response = xhr.responseText;
+      if (response === 'OK') {
+        alert('Feedback submitted successfully!');
+        form.reset();
       } else {
-        alert('Failed to send email. Please try again later.');
+        alert('Failed to submit feedback. Please try again later.');
       }
-    })
-    .catch(function(error) {
-      console.log(error);
-      alert('An error occurred. Please try again later.');
-    });
-  }
-
-  // Attach the form submission handler
-  var form = document.getElementById('contact-form');
-  form.addEventListener('submit', handleSubmit);
-</script>
+    }
+  };
+  xhr.send(formData);
+});
